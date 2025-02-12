45504e351f6554c13d6cf519038168973d1832ed
Emma Ruffet <emma.ruffet@epsi.fr>
2025-02-12 17:16:05 +0100

    server call modification

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ const usersRoutes = require('./routes/users.routes');
 app.use("/", articlesRoutes, categoriesRoutes, commentairesRoutes, contactRoutes, usersRoutes);
 
 // Configuration du serveur
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+if (require.main === module) {
+    // Ce bloc ne s'exécutera que si ce fichier est exécuté directement (pas en tant que module importé)
+    const hostname = '127.0.0.1';
+    const port = process.env.PORT || 3000;
+    app.listen(port, hostname, () => {
+        console.log(`Serveur démarré sur http://${hostname}:${port}`);
+    });
+}
 
-app.listen(port, hostname, () => {
-    console.log(`Serveur démarré sur http://${hostname}:${port}`);
-});
+module.exports = app;  // Exporte l'application pour les tests
