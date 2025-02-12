073e93b03734864624d804588fb1ac6bd0c4f6af
Emma Ruffet <emma.ruffet@epsi.fr>
2025-02-12 16:25:45 +0100

    Creating a Home Tab Test

---
+++ tests/home.test.js
@@ -0,0 +1,16 @@
+test('Home displays 3 recent articles', () => {
+    render(<Home />);
+    
+    const articles = screen.getAllByTestId('article');
+    expect(articles.length).toBe(3);
+  });
+  
+  test('See more articles button redirects to Articles', () => {
+    render(<Home />);
+    
+    const seeMoreButton = screen.getByText('Voir plus d\'articles');
+    fireEvent.click(seeMoreButton);
+  
+    expect(window.location.pathname).toBe('/articles');
+  });
+  
